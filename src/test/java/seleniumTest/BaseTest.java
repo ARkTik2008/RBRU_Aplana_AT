39b9dcc092bcaee86498d45405eb221a17ccb9fc
Nikolay MALOV <nikolay.malov@raiffeisen.ru>
2018-11-06 20:04:31 +0300

    Lesson 06_11

---
+++ src/test/java/seleniumTest/BaseTest.java
@@ -0,0 +1,69 @@
+package seleniumTest;
+
+import org.junit.After;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
+
+public class BaseTest {
+
+    WebDriverWait wait;
+    public static WebDriver driver;
+
+
+    public static WebDriver getDriver() {
+        return driver;
+    }
+
+    @BeforeAll
+    public void afterScenario() {
+        System.setProperty("webdriver.chrome.driver", "lib/chromedriver.exe");
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        options.setExperimentalOption("useAutomationExtension", false);
+
+        driver = new ChromeDriver(options);
+        wait = new WebDriverWait(driver,60);
+
+        driver.manage().timeouts().implicitlyWait(120, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(120, TimeUnit.SECONDS);
+        //driver.manage().window().maximize();
+    }
+
+    @AfterAll
+    public void afterMethod() {
+        driver.quit();
+    }
+
+    public void click(WebElement element){
+        wait.until(ExpectedConditions.visibilityOf(element)).click();
+    }
+
+    public void fillField(WebElement element, String value){
+        element.clear();
+        element.sendKeys(value);
+        element.sendKeys(Keys.TAB);
+    }
+
+    public void scrollAndClick(WebElement element){
+        JavascriptExecutor js = (JavascriptExecutor) getDriver();
+        js.executeScript("return arguments[0].scrollIntoView(false):",element);
+        wait.until(ExpectedConditions.elementToBeClickable(element)).click();
+    }
+
+    //public void selectInput(WebElement element){ }
+    //public void fillField(WebElement element){ }
+
+
+}
---
+++ src/test/java/seleniumTest/DepositTest.java
@@ -0,0 +1,17 @@
+package seleniumTest;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+
+public class DepositTest extends BaseTest {
+
+    @Test
+    public void DepositTest(){
+
+
+
+    }
+
+
+
+}
